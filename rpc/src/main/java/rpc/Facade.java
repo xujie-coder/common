0f70a9ef6ea3355f00de9506f1777111fe655108
xujie-coder <xujie_coder@163.com>
2025-01-08 17:21:43 +0800

    feat: validation的参数校验工具

---
+++ rpc/src/main/java/rpc/Facade.java
@@ -0,0 +1,10 @@
+package rpc;
+
+/**
+ * 自定义注解
+ *
+ * @author xujie
+ * @since 2025/01/08 16:51
+ */
+public @interface Facade {
+}
---
+++ rpc/src/main/java/rpc/FacadeAspect.java
@@ -0,0 +1,23 @@
+package rpc;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * Facade的切面处理类，统一进行参数校验及异常捕获
+ *
+ * @author xujie
+ * @since 2025/01/08 16:53
+ */
+@Component
+@Aspect
+@Slf4j
+public class FacadeAspect {
+
+/*    public Object facade(ProceedingJoinPoint pjp) throws Exception{
+
+    }*/
+
+}
