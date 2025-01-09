89231b292dc17157ba2b1d541e2e70cd779e86b4
xujie-coder <xujie_coder@163.com>
2025-01-09 17:24:53 +0800

    feat: Controller层统一异常处理

---
+++ web/src/main/java/vo/Result.java
@@ -0,0 +1,49 @@
+package vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import response.ResponseCode;
+import response.SingleResponse;
+
+/**
+ * @author xujie
+ * @since 2025/01/09 17:03
+ */
+@Getter
+@Setter
+public class Result<T> {
+
+    private String code;
+
+    private Boolean success;
+
+    private String message;
+
+    private T data;
+
+    public Result() {
+    }
+
+    public Result(String code, Boolean success, String message, T data) {
+        this.code = code;
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public Result(SingleResponse<T> singleResponse){
+        this.code = singleResponse.getResponseCode();
+        this.success = singleResponse.getSuccess();
+        this.message = singleResponse.getResponseMessage();
+        this.data = singleResponse.getData();
+    }
+
+    public static <T> Result <T> success(T data){
+        return new Result<>(ResponseCode.SUCCESS.name(),true,ResponseCode.SUCCESS.name(),data);
+    }
+
+    public static <T> Result <T> error(String errorCode,String errorMsg){
+        return new Result<>(errorCode,false,errorMsg,null);
+    }
+}
